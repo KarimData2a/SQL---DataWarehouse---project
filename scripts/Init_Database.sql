769b5bb2da6005a7437fd9e1fc5d6fe8b0b2b7f1
KarimData2a <Karimmarzouki2a@gmail.com>
2025-06-29 18:51:46 +0200

    Create Init_Database.sql

---
+++ scripts/Init_Database.sql
@@ -0,0 +1,12 @@
+-- Create Database 'datawarehouse'
+
+Use master;
+Create Database DataWarehouse
+
+USE [DataWarehouse];
+Create Schema bronze;
+GO
+Create Schema silver;
+GO
+Create Schema gold;
+GO
